ae2cafdde001ac0ed143002f23ed5420c93dde94
Al1Bi2 <ABC@4136>
2025-04-21 01:49:30 +0300

    redo files for fms compatibility + code cleaning

--- include/captive_portal/captive_portal.h
+++ include/captive_portal/captive_portal.h
@@ -3,7 +3,13 @@
 #include <WebServer.h>
 #include <DNSServer.h>
 #include <Preferences.h>
-//#include <ESPmDNS.h>
+#include "log.h"
+#include "fsm.h"
+
+#include <ESPmDNS.h>
+namespace captive_portal {
+    
+
 const String localIPURL = "http://esp32.local";
 const char CAPTIVE_PORTAL_HTML[] PROGMEM = R"rawliteral(
     <form action='/save' method='POST'>
@@ -13,62 +19,59 @@ const char CAPTIVE_PORTAL_HTML[] PROGMEM = R"rawliteral(
     </form>
     )rawliteral";
 
-DNSServer dnsServer;
-WebServer server(80);
-extern Preferences preferences;
+
+
 void handleRoot() {
     Serial.println("Root");
-    server.send(200, "text/html", CAPTIVE_PORTAL_HTML);
+    fsm->device.web_server->send(200, "text/html", CAPTIVE_PORTAL_HTML);
 }
-void handleSave() {String ssid = server.arg("ssid");
-    Serial.println("Save");
-    String pass = server.arg("pass");
+
+void handleSave() {
+    log_info("Saving settings...");
+    String ssid = fsm->device.web_server->arg("ssid");
+    String pass = fsm->device.web_server->arg("pass");
+
     if (ssid.length() > 0 && pass.length() > 0) {
-        preferences.putString("ssid", ssid);
-        preferences.putString("pass", pass);
-        server.send(200, "text/html", "Настройки сохранены! Перезагрузка...");
+        fsm->preferences.putString("ssid", ssid);
+        fsm->preferences.putString("pass", pass);
+        fsm->device.web_server->send(200, "text/html", "Настройки сохранены! Перезагрузка...");
         delay(1000);
-        ESP.restart();
+        fsm->post_event(DeviceEvent::REBOOT);
     }else{
-        server.send(200, "text/html", CAPTIVE_PORTAL_HTML);
+        fsm->device.web_server->send(200, "text/html", CAPTIVE_PORTAL_HTML);
     }
 }
 
-void startCaptivePortal(String hostname) {
-    Serial.println("Starting Captive Portal...");
-    WiFi.softAP(hostname);
-    dnsServer.start(53, "*", WiFi.softAPIP());
-    Serial.print("ESP32 IP as soft AP: ");
-    Serial.println(WiFi.softAPIP());
-    server.on("/", HTTP_GET,handleRoot );
+
+void startCaptivePortal() {
+    lock_ctx();
+    String id = fsm->device.id;
+    unlock_ctx();
+    WiFi.softAPConfig(IPAddress(192, 168, 0, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0));
+    WiFi.softAP(id);
+    log_info("Ip address: " + WiFi.softAPIP().toString());
+    MDNS.begin(localIPURL);
+    MDNS.addService("http", "tcp", 80);
+    lock_ctx();
+    fsm->device.web_server = new WebServer(80);
+    unlock_ctx();
+
+    fsm->device.web_server->on("/", HTTP_GET,handleRoot );
     
-    server.on("/save", HTTP_POST, handleSave);
+    fsm->device.web_server->on("/save", HTTP_POST, handleSave);
     
-    server.begin();
+    fsm->device.web_server->begin();
     while (1) {
-        dnsServer.processNextRequest();
-        server.handleClient();
+        fsm->device.web_server->handleClient();
         delay(100);
     }
-    server.stop();
-    dnsServer.stop();
+
+    fsm->device.web_server->stop();
+    MDNS.end();
+    lock_ctx();
+    delete fsm->device.web_server;
+    unlock_ctx();
+    
 }
 
-void connectToWiFi(String hostname) {
-    preferences.begin("wifi", false);
-    String ssid = preferences.getString("ssid", "");
-    String pass = preferences.getString("pass", "");
-    if(ssid.length() == 0) {
-        startCaptivePortal(hostname);
-    }
-    Serial.println("Connecting to WiFi...");
-    WiFi.begin(preferences.getString("ssid").c_str(), preferences.getString("pass").c_str());
-    for(int i = 0; i < 20 && WiFi.status() != WL_CONNECTED; i++) {
-        delay(500);
-        Serial.print(".");
-    }
-    if(!WiFi.isConnected()) {
-        startCaptivePortal(hostname);
-    }
-    Serial.println("Connected to WiFi");
 }
--- include/mqtt_handle.h
+++ include/mqtt_handle.h
@@ -5,35 +5,33 @@
 #include "cJSON.h"
 #include "OV2640.h"
 #include "fsm.h"
+
 #define LASTWILL_MESSAGE "offline"
 #define LASTWILL_QOS 1
- 
-
+extern OV2640 camera;
+namespace mqtt{
 
 #define evtADCreading      ( 1 << 3 )
 EventGroupHandle_t eg; // variable for the event group handle
-String MQTTBrokerIP = "127.0.0.1";
 
-WiFiClient      wifiClient;
-PubSubClient    mqtt_client(wifiClient);
+
+
 AMG8833 amg8833;
-extern String unique_id;
-extern OV2640 camera;
+
+
 const int AMG8833_FREQ = 2000;
 
-SemaphoreHandle_t mut_connected;
-
-volatile bool isConnected = false;
-volatile bool isActive = false;
-uint8_t notConnectedCount = 6;
 
 TaskHandle_t send_matrix_task;
 
-String CONNECT_TOPIC = String(unique_id+"/connect");
-String SETTINGS_TOPIC = String(unique_id+"/settings");
+String CONNECT_TOPIC = "/connect";
+String SETTINGS_TOPIC = "/settings";
 String CONTROL_TOPIC = "";
 String SERVER_STATUS_TOPIC = "server/status";
 String DEVICE_STATUS_TOPIC = "";
+String DISCOVERY_TOPIC = "discovery";
+String AMG8833_TOPIC = "";
+
 struct struct_message{
     char payload [500] = {'\0'};
     String topic;
@@ -43,7 +41,7 @@ QueueHandle_t q_Message;
 QueueHandle_t q_matrix;
 
 SemaphoreHandle_t sem_mqqt_busy;
-//SemaphoreHandle_t sem_mqqt_busy;
+
 
 
 
@@ -56,28 +54,6 @@ void IRAM_ATTR mqtt_callback(char* topic, byte * payload, unsigned int length){
     }
     incoming_message.payload[i] = '\0';
     xQueueOverwrite( q_Message, (void *) &incoming_message );
-    
-}
-
-void connect_mqtt(){
-    Serial.println("Connecting to MQTT...");
-    mqtt_client.setBufferSize(1000);
-    int count = 0;
-    mqtt_client.setServer(MQTTBrokerIP.c_str(), 1883);
-    String LASTWILL_TOPIC =  String(unique_id+"/status");
-    while(!mqtt_client.connected()){
-        mqtt_client.disconnect();
-        mqtt_client.connect("ESP32Client", "rmuser", "pass", LASTWILL_TOPIC.c_str(), LASTWILL_QOS, 0, LASTWILL_MESSAGE);
-        Serial.print("Attempting MQTT connection... - ");
-        Serial.println(count);
-        vTaskDelay(1000/portTICK_PERIOD_MS); 
-        if(count==5){
-            Serial.println("Restarting ESP32");
-            ESP.restart();
-        }
-    }
-    
-
     
 }
 
@@ -97,7 +73,7 @@ void parse_json_and_set_settings(const char* message){
         cJSON* item = camera_obj->child; // Получаем первый элемент объекта "camera"
         while (item) {
             if (cJSON_IsNumber(item)) { // Проверяем, что значение – число
-                camera.set_settings(item->string, item->valueint);
+                ::camera.set_settings(item->string, item->valueint);
             }
             item = item->next; // Переходим к следующему ключу
         }
@@ -132,38 +108,19 @@ void handle_message(void * parameters){
             if(incoming_message.topic == CONTROL_TOPIC){
                 log_debug("CONTROL_TOPIC");
                 if(strcmp(incoming_message.payload, "ack-connect")==0){
-                    log_info("CONNECTED");
-                    xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY );
-                    isConnected = true;
-                    vTaskSuspend(send_matrix_task);
-                    xSemaphoreGive( sem_mqqt_busy );
+                    fsm->post_event(DeviceEvent::MQTT_ACK_CONNECT);
                 }
                 if(strcmp(incoming_message.payload, "start")==0){
-                    log_info("ACTIVE");
-                    xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY );
-                    isActive = true;
-                    mqtt_client.publish(DEVICE_STATUS_TOPIC.c_str(),"active");
-                    vTaskResume(send_matrix_task);
-                    xSemaphoreGive( sem_mqqt_busy );
+                    fsm->post_event(DeviceEvent::MQTT_START_STREAM);
                 }
                 if(strcmp(incoming_message.payload,  "stop")==0){
-                    log_info("CONNECTED");
-                    xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY );
-                    vTaskSuspend(send_matrix_task);
-                    mqtt_client.publish(DEVICE_STATUS_TOPIC.c_str(),"connected");
-                    isActive = false;
-                    xSemaphoreGive( sem_mqqt_busy );
+                    fsm->post_event(DeviceEvent::MQTT_START_STREAM);
                 }
 
             }
             if(incoming_message.topic == SERVER_STATUS_TOPIC){
                 if(strcmp(incoming_message.payload,  "offline")==0 ){
-                    log_info("DISCONNECTED");
-                    xSemaphoreTake( mut_connected, portMAX_DELAY );
-                    isConnected = false;
-                    isActive = false;
-                    xSemaphoreGive( mut_connected );
-                    ESP.restart();
+                    fsm->post_event(DeviceEvent::SERVER_OFFLINE);
                 }
             }
 
@@ -183,20 +140,11 @@ void loop_mqtt(void * parameters) {
     TickType_t xLastWakeTime = xTaskGetTickCount();
     const TickType_t xFrequency = pdMS_TO_TICKS(500); 
     while(true){
-        //if ( (wifiClient.connected()) && (WiFi.status() == WL_CONNECTED) )
-       // {
         xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY ); 
-        mqtt_client.connected();
-        mqtt_client.loop();
+        fsm->device.mqtt_client.connected();
+        fsm->device.mqtt_client.loop();
         xSemaphoreGive( sem_mqqt_busy );
-       /* }
-        else {
-            if ( !(wifiClient.connected()) || !(WiFi.status() == WL_CONNECTED) )
-            {
-                connectToWiFi(unique_id);
-            }
-            connect_mqtt();
-        }*/
+
         taskYIELD();
         xLastWakeTime = xTaskGetTickCount();
         vTaskDelayUntil( &xLastWakeTime, xFrequency );
@@ -232,76 +180,113 @@ void send_matrix(void * parameters) {
             xEventGroupSetBits( eg, evtADCreading );
         }
         if(xQueueReceive(q_matrix, &r_matrix, 0) == pdTRUE){
-            uint32_t t1 = micros();
-            cJSON *root = cJSON_CreateObject();
-            cJSON *matrix_array = cJSON_CreateArray();
-            //Serial.println("Recieved matrix");
             
-            for(int i = 0; i < 8; i++){
-                cJSON *row_array = cJSON_CreateArray();
-                for(int j = 0; j < 8; j++){
-                    cJSON_AddItemToArray(row_array, cJSON_CreateNumber(r_matrix[i][j]));
-                }
-                cJSON_AddItemToArray(matrix_array, row_array);
-            }
-            cJSON_AddItemToObject(root, "matrix", matrix_array);
-            char *json_string = cJSON_PrintUnformatted(root);
-            
-
             xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY );
-            String topic = unique_id + "/amg8833";
-            log_debug("Sending matrix");
-            mqtt_client.publish(topic.c_str(), reinterpret_cast<const uint8_t*>(&r_matrix), sizeof(r_matrix));
-            uint32_t t2 = micros();
-
+            fsm->device.mqtt_client.publish(AMG8833_TOPIC.c_str(), reinterpret_cast<const uint8_t*>(&r_matrix), sizeof(r_matrix));
             xSemaphoreGive( sem_mqqt_busy );
-            cJSON_Delete(root);
-            free(json_string);  
+
         }
             
         taskYIELD();
     }
 
 }
+void setup_amg8833() {
+    lock_ctx();
+    fsm->device.amg8833 = AMG8833();
+    unlock_ctx();
+    fsm->device.amg8833.init(); //shiiiiiiet
+    fsm->device.amg8833.reset();
+    fsm->device.amg8833.set_framerate(FRAMERATE::FPS_10);
+}
+void update_topic_names(){
+    lock_ctx();
+    String id = fsm->device.id;
+    unlock_ctx();
+    SETTINGS_TOPIC = String(id+"/settings");
+    CONTROL_TOPIC = id+"/control";
+    DEVICE_STATUS_TOPIC = id+"/status";
+    AMG8833_TOPIC = id+"/amg8833";
+}
 void setup_mqtt() {
-    amg8833.init(); //shiiiiiiet
-    amg8833.reset();
-    amg8833.set_framerate(FRAMERATE::FPS_10);
+
     eg = xEventGroupCreate();
     q_matrix = xQueueCreate( 1, sizeof(std::array<std::array<float, 8>, 8>) );
     q_Message = xQueueCreate( 1, sizeof(struct struct_message) );
-    mut_connected = xSemaphoreCreateMutex();
-    Serial.printf("Size of matrix: %d\n",sizeof(std::array<std::array<float, 8>, 8>)); 
-    mqtt_client.setCallback(mqtt_callback);
 
+    update_topic_names();
 
-    SETTINGS_TOPIC = String(unique_id+"/settings");
-    mqtt_client.subscribe(SETTINGS_TOPIC.c_str(),1);
+    fsm->device.mqtt_client.setCallback(mqtt_callback);
 
-    CONTROL_TOPIC = unique_id+"/control";
-    DEVICE_STATUS_TOPIC = unique_id+"/status";
+    fsm->device.mqtt_client.subscribe(SETTINGS_TOPIC.c_str(),1);
+    fsm->device.mqtt_client.subscribe(SERVER_STATUS_TOPIC.c_str(),1);
+    fsm->device.mqtt_client.subscribe(DEVICE_STATUS_TOPIC.c_str(),1);
+    fsm->device.mqtt_client.subscribe(CONTROL_TOPIC.c_str(),1);
 
-    mqtt_client.subscribe(SERVER_STATUS_TOPIC.c_str(),1);
-    mqtt_client.subscribe(DEVICE_STATUS_TOPIC.c_str(),1);
-    mqtt_client.subscribe(CONTROL_TOPIC.c_str(),1);
+    
+    
+    xTaskCreatePinnedToCore(gett_matrix, "gett_matrix", 4096, fsm, 3, NULL, 1);
+    xTaskCreatePinnedToCore(send_matrix, "send_matrix", 4096, fsm, 2, &send_matrix_task, 1);
 
-
-    xTaskCreatePinnedToCore(loop_mqtt, "loop_mqtt", 4906, NULL, 2, NULL, 1);
-    xTaskCreatePinnedToCore(handle_message, "handle_message", 4096, NULL, 2, NULL, 1);
-    xTaskCreatePinnedToCore(gett_matrix, "gett_matrix", 4096, NULL, 3, NULL, 1);
-    xTaskCreatePinnedToCore(send_matrix, "send_matrix", 4096, NULL, 2, &send_matrix_task, 1);
+    xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY );
     vTaskSuspend(send_matrix_task);
     xSemaphoreGive( sem_mqqt_busy );
 
-    log_debug(CONTROL_TOPIC);
-    log_debug(DEVICE_STATUS_TOPIC);
-    log_debug(SERVER_STATUS_TOPIC);
-    log_debug(SETTINGS_TOPIC);
-    String message = unique_id+":"+WiFi.localIP().toString();
-    
+}
 
-    Serial.println(mqtt_client.publish("discovery", message.c_str()));
+void start_stream(){
+    xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY );
+    vTaskResume(send_matrix_task);
+    fsm->device.mqtt_client.publish(DEVICE_STATUS_TOPIC.c_str(), "active");
+    xSemaphoreGive( sem_mqqt_busy );
 
+}
+void stop_stream(){
+    xSemaphoreTake( sem_mqqt_busy, portMAX_DELAY );
+    vTaskSuspend(send_matrix_task);
+    fsm->device.mqtt_client.publish(DEVICE_STATUS_TOPIC.c_str(), "connected");
+    xSemaphoreGive( sem_mqqt_busy );
 }
 
 
+void connectMqtt(){
+    int count = 0;
+    lock_ctx();
+    fsm->device.wifi = WiFiClient();
+    fsm->device.mqtt_client = PubSubClient(fsm->device.wifi);
+    unlock_ctx();
+    fsm->device.mqtt_client.setServer(fsm->device.server_ip.c_str(), 1883);
+    Serial.print("Attempting MQTT connection... - ");
+    while(!fsm->device.mqtt_client.connected()){
+        fsm->device.mqtt_client.disconnect();
+        fsm->device.mqtt_client.connect("ESP32Client", "rmuser", "pass", DEVICE_STATUS_TOPIC.c_str(), LASTWILL_QOS, 0, LASTWILL_MESSAGE);
+        Serial.println(count);
+        vTaskDelay(1000/portTICK_PERIOD_MS); 
+        if(count++ == 10){
+            log_info("Restarting ESP32");
+            fsm->post_event(DeviceEvent::MQTT_FAIL);
+        }
+    }
+
+    fsm->post_event(DeviceEvent::MQTT_OK);
+}
+
+void discoverServer(){
+    fsm->device.mqtt_client.subscribe(SERVER_STATUS_TOPIC.c_str());
+    DeviceState state;
+    lock_ctx();
+    String id = fsm->device.id;
+    String ip = fsm->device.ip;
+    unlock_ctx();
+    String message = id+":"+ip;
+    do{
+        fsm->device.mqtt_client.publish(DISCOVERY_TOPIC.c_str(), message.c_str());
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        lock_ctx();
+        state = fsm->get_current_state();
+        unlock_ctx();
+    }while(state != DeviceState::REGISTERED);
+}
+
+
+}
